490ec5f85f3ba4a04b59afdea32db3e0b010a650
Sergio Martinez <dialstudios@gmail.com>
2017-03-23 12:39:02 -0500

    Add advance front col one add slider

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,1 @@
-$(function(){});
+jQuery(document).ready(function(i){i(".slider").slick({infinite:!0,slidesToShow:3,slidesToScroll:3})});
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
-/*jshint strict:true, browser:true, jquery:true */
-$(function() {
-
+jQuery(document).ready(function($) {
+    // slider slick home
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 });
