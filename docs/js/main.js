9c58052a8f740083bf919fdd27004df86e08e572
Sergio Martinez <dialstudios@gmail.com>
2017-04-04 13:16:35 -0500

    Add Front final v.0.1 front site

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,35 @@
-jQuery(document).ready(function(i){i(".slider").slick({infinite:!0,slidesToShow:3,slidesToScroll:3,autoplay:!0})});
+jQuery(document).ready(function($) {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 15000,
+        arrows: false,
+        resposive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+});
